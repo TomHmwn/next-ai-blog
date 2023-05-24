9a034b23d72d4e75d136a13e93db954c91b47aec
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-05-24 15:33:11 +0700

    add tech, travel, trending, Card for preview of articles, footer, other and subscribe components into homepage

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -6,7 +6,33 @@ const Footer = () => {
       <div className='justify-between mx-auto gap-16 sm:flex'>
         {/* FIRST COLLUMN */}
         <div className='mt-16 basis-1/2 sm:mt-0'>
-
+          <h4 className='font-bold'>BLOG OF THE FUTURE</h4>
+          <p className="my-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius et iure ex! Esse distinctio ut eaque iure dicta unde repellat quis, vitae eligendi harum voluptas in officiis accusamus placeat asperiores!
+          </p>
+          <p>copyright Blog of the Futre All Rights Reserved.</p>
+        </div>
+        {/* SECOND COLLUMN */}
+        <div className='mt-16 basis-1/4 sm:mt-0'>
+          <h4 className='font-bold'>Links</h4>
+          <p className="my-5">
+            Massa Orna Snolaparo
+          </p>
+          <p className="my-5">
+            Some random link
+          </p>
+          <p>Ullmacorperater visacomando</p>
+        </div>
+        {/* THIRD COLLUMN */}
+        <div className='mt-16 basis-1/4 sm:mt-0'>
+          <h4 className='font-bold'>Contact Us</h4>
+          <p className="my-5">
+            Massa Orna Snolaparo
+          </p>
+          <p className="my-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+          <p>(222)481-739</p>
         </div>
       </div>
     </footer>
